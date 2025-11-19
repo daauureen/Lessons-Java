8a38312152de4e4a387337744dbec587236bf473
daauureen <daurenbolatofficial@gmail.com>
2025-11-19 16:01:38 +0500

    upd

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        // main это стартовая точка любой программы
         System.out.println("Hello, world!");
+        System.out.print("Hi");
+        System.out.print(" ");
+        System.out.printf("heeeell nah");
     }
 }
---
+++ src/Variables.java
@@ -0,0 +1,24 @@
+public class Variables {
+    public static void main(String[] arguments) {
+        int myInt = 100;
+        short myShort = 12;
+        long myLong = 12344;
+
+        double myDouble = 12.3;
+        float myFloat = 123.43f;
+
+        char myChar = 'a';
+        boolean myBoolean = false;
+
+        byte myByte = 100;
+        
+        System.out.println(myInt);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+    }
+}
