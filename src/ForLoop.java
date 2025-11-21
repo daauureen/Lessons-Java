18fdb9e628d4d58d8a2d43e4b75e36bfc0d82efa
daauureen <daurenbolatofficial@gmail.com>
2025-11-21 16:22:43 +0500

    upd

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,7 +1,61 @@
+// public class ForLoop {
+//     public static void main(String[] args) {
+//         for (int i = 0; i <= 10; i++) {
+//             System.out.println("Hello");
+//         }
+//     }
+// }
+
+
+
+// public class ForLoop {
+//     public static void main(String[] args) {
+//         for (int i = 0; i <= 10; i++) {
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
+
+// public class ForLoop {
+//     public static void main(String[] args) {
+//         for (int i = 2; i <= 20; i = i + 2) {
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
+
+// public class ForLoop {
+//     public static void main(String[] args) {
+//         int sum = 0;
+//         for (int i = 0; i <= 100; i++) {
+//             sum = sum + i;
+//         }
+//         System.out.println("sum is " + sum);
+//     }
+// }
+
+
+
+// public class ForLoop {
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 10; i++) {
+//             System.out.println("5 * " + i + " = " + (5 * i));
+//         }
+//     }
+// }
+
+
+
 public class ForLoop {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
-            System.out.println("Hello");
+        int factorial = 1;
+        for (int i = 1; i <= 9; i++) {
+            factorial = factorial * i;
         }
+        System.out.println(factorial);
     }
 }
---
+++ src/ScannerClass.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ScannerClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input something");
+        String string = sc.nextLine();
+        System.out.println("You inputted: " + string);
+    }
+}
