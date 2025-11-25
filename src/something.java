7c42c759d6a72c661a83a71dcaa84411eb87661f
daauureen <daurenbolatofficial@gmail.com>
2025-11-25 18:25:31 +0500

    upd

--- src/something.java
+++ src/something.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class something {
     public static void main(String[] args) {
-        int counter;
-        counter = 0;
-        System.out.println(counter++);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input: ");
+        String dauren = scanner.nextLine();
+        System.out.println("u wrote u are " + dauren);
     }
 }
