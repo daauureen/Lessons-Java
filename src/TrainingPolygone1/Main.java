49696a950c42e1f21d2c52ca877c26514a44cd71
daauureen <daurenbolatofficial@gmail.com>
2025-12-04 18:10:08 +0500

    upd

--- src/TrainingPolygone1/Main.java
+++ src/TrainingPolygone1/Main.java
@@ -1,15 +1,13 @@
 package TrainingPolygone1;
 
-import java.util.Arrays;
 import java.util.Scanner;
-public class Main {
 
+class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Input some text: ");
-        String names = sc.nextLine();
-        String[] splitNames = names.split(", ");
-        System.out.println(Arrays.toString(splitNames));
-        sc.close();
+        Scanner scanner = new Scanner(System.in);
+        
+        // System.out.println("Hello\\ \"World\" \nmy niga \tgo");
+
+        
     }
 }
